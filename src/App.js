0e2f6a579ab59ff58bd9db67c24a70e0ee3db06f
ivan.silva <ivan.silva@vilanova.com.br>
2020-07-30 08:31:00 -0300

    listar produtos

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import api from './services/api';
+//import api from './services/api';
 import './styles.css'
 import Header from './components/Header';
 import Main from './pages/main';
